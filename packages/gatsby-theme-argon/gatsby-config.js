43598d276c96f0e50f9417053c4ab258478c7ff5
Jason Quach <thuanqh@gmail.com>
2019-12-25 19:31:50 +0700

    Add package for gatsby-theme-agon

---
+++ packages/gatsby-theme-argon/gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-theme-ui',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx'
+  ]
+};

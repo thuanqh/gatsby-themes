c93f5c573bd07d5f21e0adc73f85f5c9fe79d04b
Jason Quach <jason@lungvang.com>
2019-12-28 10:04:12 +0700

    Init landing page

--- packages/gatsby-theme-argon/src/gatsby-plugin-theme-ui/index.js
+++ packages/gatsby-theme-argon/src/gatsby-plugin-theme-ui/index.js
@@ -1,1 +1,8 @@
-export default {};
+import { tailwind } from '@theme-ui/presets';
+
+export default {
+  ...tailwind,
+  styles: {
+    ...tailwind
+  }
+};
---
+++ packages/gatsby-theme-argon/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Heading } from '@theme-ui/components';
+
+export default () => {
+  return (
+    <Box>
+      <Heading>Landing Page</Heading>
+    </Box>
+  );
+};

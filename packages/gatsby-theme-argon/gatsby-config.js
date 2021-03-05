b21842bd152949331dc4bc29df4719168e276c92
Jason Quach <thuanqh@gmail.com>
2021-03-05 12:52:48 +0700

    Update package.json

--- packages/gatsby-theme-argon/gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    'gatsby-plugin-theme-ui',
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-mdx'
-  ]
-};
---
+++ packages/gatsby-theme-blog-core/README.md
@@ -0,0 +1,11 @@
+# `gatsby-theme-blog-core`
+
+> TODO: description
+
+## Usage
+
+```
+const gatsbyThemeBlogCore = require('gatsby-theme-blog-core');
+
+// TODO: DEMONSTRATE API
+```
